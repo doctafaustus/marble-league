23213590e79abce9a74bbc1ff3fb581745035081
doctafaustus <clowd150@gmail.com>
2017-05-14 19:13:23 -0500

    Add private folder

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 // MODULES
 const express = require('express');
+const bodyParser = require('body-parser');
+const fs = require('fs');
 
 // STRIPE
 const stripeSK = process.env.PORT ? process.env.STRIPE_LIVE_SK : process.env.STRIPE_TEST_SK;
 const stripe = require('stripe')(stripeSK);
+
+console.log(process.env);
 
 // DATABASE
 const mongoose = require('mongoose');
@@ -33,12 +37,12 @@ const Marble = mongoose.model('Marble', new Schema({
 const app = express();
 app.use(express.static(__dirname + '/public'));
 app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: true, limit: '1mb' }));
 
-
+// Home page
 app.get('/', function(req, res) {
-
 	Marble.find({}).sort({color: -1}).exec(function (err, marbles) {
-  	if (err) throw err;
+  	if (err) console.log(err);
 
   	const availableMarbles = marbles.filter(function(marble) {
   		return !marble.userGivenName;
@@ -50,8 +54,41 @@ app.get('/', function(req, res) {
 
   	res.render('index.ejs', { availableMarbles: availableMarbles });
   });
-
 });
+
+// Stripe charge
+app.post('/charge', function(req, res) {
+	console.log('/charge');
+	console.log(req.body);
+
+  stripe.customers.create({
+	  description: 'Customer: ' + req.body.stripeEmail,
+	  source: req.body.stripeToken,
+	  email: req.body.stripeEmail,
+	}, function(err, customer) {
+		if (err) console.log(err);
+		console.log(customer);
+		stripe.charges.create({
+		  amount: 500,
+		  currency: 'usd',
+		  customer: customer.id,
+		  description: 'Charge for ' + req.body.stripeEmail,
+		}, function(err, charge) {
+			if (err) console.log(err);
+			Marble.find({ color: req.body.marbleColor }).exec(function (err, marble) {
+		  	if (err) console.log(err);
+		  	marble.userGivenName = req.body.userGivenName;
+				marble.save(function(err) {
+          if (err) console.log(err);
+					console.log('Charge created for ' + marble.color);
+					res.sendStatus(200);
+        });
+		  });
+	  });
+	});
+});
+
+
 
 
 app.listen(process.env.PORT || 3000, function() {
